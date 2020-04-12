2b138ed637d3feb0bc8b255d79ba853e3cf4c96c
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-12 22:23:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def find_min_in_nested_arrays(src)
         element_index += 1
 
       end
-      outer_results << lowest_value
+      outer_results.push(lowest_value)
       row_index += 1
 
   end
